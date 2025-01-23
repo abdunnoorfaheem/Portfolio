ffcefd302a361de7fd68188c7e77c8d102522bb2
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-01-23 18:24:46 +0600

    react portfolio project setup done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <section>
+        <h1>Home</h1>
+    </section>
+    </>
+  )
+}
+
+export default Home
