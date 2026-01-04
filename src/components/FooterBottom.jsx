f15e7c822dbcba18b9659fd2b2cf62d0480c7a58
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-01-04 22:27:41 +0600

    copyright year update

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -6,7 +6,7 @@ const FooterBottom = () => {
     
     <section className=" bg-gradient-to-r from-gray-900 to-gray-800 text-white  ">
     <div className="container mx-auto pt-0 pb-8">
-    <div className="text-center text-xl md:text-4xl">&copy;2025 All Rights Reserved by <i className="font-bold md:text-5xl">Abdun Noor</i></div>
+    <div className="text-center text-xl md:text-4xl">&copy;2026 All Rights Reserved by <i className="font-bold md:text-5xl">Abdun Noor</i></div>
     </div>
     </section>
     
